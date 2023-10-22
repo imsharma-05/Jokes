596b5c8b1220fa2186a41ba135b0bdbf708b9013
Satyam <s.sharma9754649@gmail.com>
2023-10-22 15:55:33 +0530

    first commit

---
+++ Jokes/jokes/script.js
@@ -0,0 +1,28 @@
+// PROMISES
+// includes then, then...;
+// fulfilled
+// error
+
+
+
+const jokes = document.querySelector('#joke');
+const jokeBtn = document.querySelector('#jokeBtn');
+
+function generateJokes(){
+
+    const setHeader ={
+        headers: {
+            Accept : "application/json"
+        }
+    }
+    fetch('https://icanhazdadjoke.com/', setHeader).then((res)=>res.json())
+    .then((data)=>{
+        jokes.innerHTML = data.joke;
+    }).catch((error)=>{
+        console.log(error);
+    }) 
+}
+
+
+jokeBtn.addEventListener('click', generateJokes);
+generateJokes();
